2ac9dbbb6c640c88f5665d5f50c70f8d77dbb2bb
Soumotanu Mazumdar <msoumotanu@gmail.com>
2020-09-21 19:56:49 +0530

    Search bar completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React from 'react';
+import SearchBar from './SearchBar'
 
-const App = () => {
-  return <h1>Hello</h1>;
+class App extends React.Component {
+  onSearchSubmit(term) {
+    console.log(term);
+  }
+
+  render () {
+    return (
+      <div className="ui container" style={{ marginTop: "10px" }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+      </div>
+    );
+  }
 };
 
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,5 +1,32 @@
-import React from 'react';
+import React from "react";
 
-class ImageList extends React.Component {
-  render()
+class SearchBar extends React.Component {
+  state = { term: "" };
+
+  onFormSubmit = (event) => {
+    event.preventDefault();
+    this.props.onSubmit(this.state.term);
+    this.setState({ term: ''})
+  };
+
+  render() {
+    return (
+      <div className="ui segment">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
+          <div className="field">
+            <label>Search Image</label>
+            <input
+              type="text"
+              name="search"
+              placeholder="Search"
+              value={this.state.term}
+              onChange={(event) => this.setState({ term: event.target.value.trimStart() })}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
+
+export default SearchBar;
