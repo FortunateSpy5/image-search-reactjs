054980a4c658094933f3ef40f3c66061ec211294
Soumotanu Mazumdar <msoumotanu@gmail.com>
2020-09-21 20:53:24 +0530

    Added unsplash API

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,48 @@
-import React from 'react';
-import SearchBar from './SearchBar'
+import React from "react";
+import axios from "axios";
+import SearchBar from "./SearchBar";
+import ImageList from "./ImageList";
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    console.log(term);
+  state = {
+    images: [],
+    foundMessage: ''
   }
 
-  render () {
+   onSearchSubmit = async (term) => {
+    try {
+      const response = await axios.get(
+        "https://api.unsplash.com/search/photos",
+        {
+          params: {
+            query: term,
+          },
+          headers: {
+            Authorization:
+              "Client-ID 8EjyvMwSklYrEd1YqP_uxltqPjimDJOHMQHhVuFOnQ8",
+          },
+        }
+      );
+
+      this.setState({ images: response.data.results });
+      this.setState({
+        foundMessage: `Found ${this.state.images.length} Images`
+      });
+    }
+    catch (error) {
+      console.log(error.message);
+    }
+  }
+
+  render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        {this.state.foundMessage}
+        <ImageList images={this.state.images} />
       </div>
     );
   }
-};
+}
 
 export default App;
---
+++ src/components/ImageList.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class ImageList extends React.Component {
+  render() {
+    return (
+      <div className="ui segment">
+      </div>
+    );
+  }
+}
+
+export default ImageList;
