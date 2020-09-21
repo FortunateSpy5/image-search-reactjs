ab558f940cd1ca7ab4c6ab9d4c8ccd9a962d80ee
Soumotanu Mazumdar <msoumotanu@gmail.com>
2020-09-22 01:55:55 +0530

    Updates

---
+++ precache-manifest.a7080f65bb547c5f63e9b1de2a3f739a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83d5648816c5ea4d3dbca7c95b655200",
+    "url": "/image-search-reactjs/index.html"
+  },
+  {
+    "revision": "b1e99a6d7d3c37b5805f",
+    "url": "/image-search-reactjs/static/css/main.73e43168.chunk.css"
+  },
+  {
+    "revision": "a73d070b1526619d8bb9",
+    "url": "/image-search-reactjs/static/js/2.0edc1e99.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/image-search-reactjs/static/js/2.0edc1e99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1e99a6d7d3c37b5805f",
+    "url": "/image-search-reactjs/static/js/main.4773fa1d.chunk.js"
+  },
+  {
+    "revision": "bc34810d7f9dcafd6264",
+    "url": "/image-search-reactjs/static/js/runtime-main.a1fe03ad.js"
+  }
+]);
